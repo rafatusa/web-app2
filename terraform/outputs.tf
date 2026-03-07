fce41cab44bff9183642155bc1d07096d59637e0
rafatusa <piealrafat@gmail.com>
2026-03-06 20:36:27 -0500

    Update via DevOps Agent

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,29 +1,34 @@
 output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.web_app_instance.id
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.web_docker.id
 }
 
 output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web_app_instance.public_ip
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.web_docker.public_ip
 }
 
 output "instance_public_dns" {
-  description = "Public DNS name of the EC2 instance"
-  value       = aws_instance.web_app_instance.public_dns
+  description = "The public DNS name of the EC2 instance"
+  value       = aws_instance.web_docker.public_dns
 }
 
 output "nginx_url" {
-  description = "URL to access the nginx web server"
-  value       = "http://${aws_instance.web_app_instance.public_ip}"
+  description = "The URL to access the nginx web server"
+  value       = "http://${aws_instance.web_docker.public_ip}"
+}
+
+output "ssh_connection" {
+  description = "SSH connection string for the EC2 instance"
+  value       = "ssh -i ${var.key_name}.pem ec2-user@${aws_instance.web_docker.public_ip}"
 }
 
 output "security_group_id" {
-  description = "ID of the security group attached to the EC2 instance"
-  value       = aws_security_group.web_app_sg.id
+  description = "The ID of the security group"
+  value       = aws_security_group.web_docker_sg.id
 }
 
-output "ssh_connection_string" {
-  description = "SSH connection string for the EC2 instance"
-  value       = "ssh -i ${var.key_name}.pem ec2-user@${aws_instance.web_app_instance.public_ip}"
+output "instance_state" {
+  description = "The state of the EC2 instance"
+  value       = aws_instance.web_docker.instance_state
 }
