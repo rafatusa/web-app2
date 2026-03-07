86039cf32257dfe565553c411067c5d9925ccb16
rafatusa <piealrafat@gmail.com>
2026-03-06 20:36:26 -0500

    Update via DevOps Agent

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,10 +4,28 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair to use for the EC2 instance"
+  type        = string
+  default     = "web-docker-key"
+}
+
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "web-app2"
+  default     = "web-docker"
 }
 
 variable "environment" {
@@ -16,68 +34,38 @@ variable "environment" {
   default     = "production"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type for nginx server"
-  type        = string
-  default     = "t3.micro"
-}
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance (Amazon Linux 2)"
-  type        = string
-  default     = "ami-0c02fb55956c7d316"
-}
-
-variable "key_name" {
-  description = "Name of the SSH key pair to use for EC2 instance"
-  type        = string
-  default     = ""
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "availability_zone" {
-  description = "Availability zone for the subnet"
-  type        = string
-  default     = "us-east-1a"
-}
-
 variable "nginx_port" {
   description = "Port on which nginx will listen"
   type        = number
   default     = 80
 }
 
-variable "nginx_ssl_port" {
-  description = "Port on which nginx will listen for HTTPS"
+variable "nginx_https_port" {
+  description = "HTTPS port on which nginx will listen"
   type        = number
   default     = 443
 }
 
+variable "ssh_port" {
+  description = "Port for SSH access"
+  type        = number
+  default     = 22
+}
+
 variable "allowed_ssh_cidr" {
-  description = "CIDR block allowed to SSH into the EC2 instance"
+  description = "CIDR block allowed to SSH into the instance"
   type        = string
   default     = "0.0.0.0/0"
 }
 
 variable "allowed_http_cidr" {
-  description = "CIDR block allowed to access nginx via HTTP"
+  description = "CIDR block allowed to access HTTP"
   type        = string
   default     = "0.0.0.0/0"
 }
 
 variable "allowed_https_cidr" {
-  description = "CIDR block allowed to access nginx via HTTPS"
+  description = "CIDR block allowed to access HTTPS"
   type        = string
   default     = "0.0.0.0/0"
 }
@@ -91,13 +79,19 @@ variable "root_volume_size" {
 variable "root_volume_type" {
   description = "Type of the root EBS volume"
   type        = string
-  default     = "gp3"
+  default     = "gp2"
 }
 
-variable "associate_public_ip" {
-  description = "Whether to associate a public IP address with the EC2 instance"
-  type        = bool
-  default     = true
+variable "docker_image" {
+  description = "Docker image to use for nginx"
+  type        = string
+  default     = "nginx:latest"
+}
+
+variable "container_name" {
+  description = "Name of the nginx Docker container"
+  type        = string
+  default     = "web-docker-nginx"
 }
 
 variable "tags" {
